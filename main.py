6399f9eb709b498db82c9ccb5a1493e106962b9e
MateoDe <mateodellacasa00@gmail.com>
2020-11-17 23:09:56 -0300

    Fix Error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from dolar_price import get_price
 import telebot
-
+from time import sleep
 
 DOLAR_BLUE_URL = 'https://www.cronista.com/MercadosOnline/dolar.html'
 COMPRA_XPATH = '//*[@id="dcompra1"]/div[1]/text()'
@@ -12,11 +12,15 @@ bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=['precio'])
-def price_message(message):
+def price_message(message):	
+	price = get_price(DOLAR_BLUE_URL, COMPRA_XPATH, VENTA_XPATH)
 	try:
-		price = get_price(DOLAR_BLUE_URL, COMPRA_XPATH, VENTA_XPATH)
-		sleep(1)
-		bot.reply_to(message, f'Precio de COMPRA: {price[0]} \nPrecio de VENTA: {price[1]} \nFUENTE: El Cronista \nAceptamos donaciones para mantener el bot en linea:\nBTC: 17oyXUTzgcznNjgw3eN72vsHHFTptKFQx1 \nETH: 0xd0e693bb20a99950777e6cf6883574e6c35c1709 \n GitHub: MateoDe')
+		bot.reply_to(message, f'Precio de COMPRA: {price[0]} \nPrecio de VENTA: {price[1]} \nFUENTE: El Cronista \n \nAceptamos donaciones para mantener el bot en linea:\nBTC: 17oyXUTzgcznNjgw3eN72vsHHFTptKFQx1 \nETH: 0xd0e693bb20a99950777e6cf6883574e6c35c1709 \nGitHub: MateoDe')
 	except:
-		bot.reply_to(message, 'Intenta nuevamente en unos segundos...')
+		try:
+			sleep(1)
+			bot.reply_to(message, f'Precio de COMPRA: {price[0]} \nPrecio de VENTA: {price[1]} \nFUENTE: El Cronista')
+		except:
+			sleep(5)
+
 bot.polling()
