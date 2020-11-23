a12b24babb324dec06b8ec52abb7540446a20b95
MateoDe <mateodellacasa00@gmail.com>
2020-11-23 18:19:00 -0300

    Cambio de fuente

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from dolar_price import get_price
 import telebot
 from time import sleep
 
-DOLAR_BLUE_URL = 'https://www.cronista.com/MercadosOnline/dolar.html'
-COMPRA_XPATH = '//*[@id="dcompra1"]/div[1]/text()'
-VENTA_XPATH = '//*[@id="dventa1"]/div[1]/text()'
+DOLAR_BLUE_URL = 'https://dolarhoy.com/'
+COMPRA_XPATH = '/html/body/div/div/div/div[1]/div[1]/div[2]/div/div[1]/div[1]/span/text()'
+VENTA_XPATH = '/html/body/div/div/div/div[1]/div[1]/div[2]/div/div[1]/div[2]/span/text()'
 TOKEN = input('Token de telegram: ')
 
 
@@ -15,11 +15,11 @@ bot = telebot.TeleBot(TOKEN)
 def price_message(message):	
 	price = get_price(DOLAR_BLUE_URL, COMPRA_XPATH, VENTA_XPATH)
 	try:
-		bot.reply_to(message, f'Precio de COMPRA: {price[0]} \nPrecio de VENTA: {price[1]} \nFUENTE: El Cronista \n \nAceptamos donaciones para mantener el bot en linea:\nBTC: 17oyXUTzgcznNjgw3eN72vsHHFTptKFQx1 \nETH: 0xd0e693bb20a99950777e6cf6883574e6c35c1709 \nGitHub: MateoDe')
+		bot.reply_to(message, f'Precio de COMPRA: {price[0]} \nPrecio de VENTA: {price[1]} \nFUENTE: DolarHoy.com')
 	except:
 		try:
 			sleep(1)
-			bot.reply_to(message, f'Precio de COMPRA: {price[0]} \nPrecio de VENTA: {price[1]} \nFUENTE: El Cronista')
+			bot.reply_to(message, f'Precio de COMPRA: {price[0]} \nPrecio de VENTA: {price[1]} \nFUENTE: DolarHoy.com')
 		except:
 			sleep(5)
 
